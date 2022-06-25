bb8a7d8fbe0fb7e6319ec0ce76cbbd716da84f61
juan <juan.fuentes1111@misena.edu.co>
2022-06-24 22:29:52 -0500

    asdasdsad

---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+
+    <v-toolbar app class="warning">
+      <v-app-bar-nav-icon @click="drawer=!drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+
+    <v-navigation-drawer app v-model="drawer" temporary dark>
+      <h3>menu</h3>
+    </v-navigation-drawer>
+
+    </v-toolbar>
+
+</template>
+
+<script>
+export default {
+    name:"InicioHeader",
+    data: () => ({
+      drawer: true,
+    })
+
+  }
+  //https://i.ibb.co/fktCG83/asdasdasdasdsadsadsa.png
+</script>
+
+<style>
+#b {
+  height: 500px;
+  width: 100vw;
+  background-image: url("https://i.ibb.co/fktCG83/asdasdasdasdsadsadsa.png")
+}
+
+</style>
